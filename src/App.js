1d03b8c8a02b60ff0221173a200b43182b7c839a
Satrio Kamaludin <satriokamaludin@gmail.com>
2023-06-21 07:00:49 +0700

    Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './components/Header';
 
 const App = () => {
   return (
     <div>
+      <Header />
       Hello World
     </div>
   )
